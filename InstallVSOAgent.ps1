5d1bd56394d80059ad96bb9c3bc742791228eca0
Mariusz Kolodziej <makolo@microsoft.com>
2017-10-03 23:27:58 -0400

    updated with node.js and npm install

--- InstallVSOAgent.ps1
+++ InstallVSOAgent.ps1
@@ -29,6 +29,21 @@ foreach ($disk in $disks) {
 $count++
 }
 
+# How to install Chocolatey - https://chocolatey.org/install
+# How to install node.js with Chocolatey - https://chocolatey.org/packages/nodejs.install
+# Check PS ExecutionPolicy for Chocolatey Install
+Write-Verbose "Checking ExecutionPolicy" -verbose
+Get-ExecutionPolicy
+
+# Get and Install Chocolatey
+Write-Verbose "Getting and Installing Chocolatey" -verbose
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Get and Install Node.js with Chocolatey
+Write-Verbose "Getting and Installing Node.js with Chocolatey" -verbose
+choco install nodejs.install -y
+
+# Start VSO Agent Install
 Write-Verbose "Entering InstallVSOAgent.ps1" -verbose
 
 $currentLocation = Split-Path -parent $MyInvocation.MyCommand.Definition
